0a370da44fb68382fcacb15eb67fa27961389c95
Ignacio Crotez <ignacio.cortez@kodigo.org>
2023-09-01 16:11:23 -0600

    feat: creating new comment route

--- src/routes/comments.routes.ts
+++ src/routes/comments.routes.ts
@@ -4,6 +4,7 @@ import {
   createComment,
   editCommentById,
   deleteCommentById,
+  getCommentsById,
 } from "../controller/commentsController";
 
 const router = Router();
@@ -11,6 +12,8 @@ const router = Router();
 router.post("/comments", createComment);
 
 router.get("/comments", getComments);
+
+router.get("/comments/:id", getCommentsById);
 
 router.put("/comments/:id", editCommentById);
 
